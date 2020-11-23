b9d6173dd01e87721e63e23c9e3ad96567a274c0
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-11-23 20:29:52 +0200

    added solved task-01

---
+++ task-01.js
@@ -0,0 +1,11 @@
+const navRef = document.querySelector('#categories');
+console.log(`В списке ${navRef.children.length} категории`);
+
+const itemsRef = document.querySelectorAll('.item');
+const itemsArray = [...itemsRef].map(item =>{
+  console.log (`Категория: ${item.children[0].textContent}`);
+  console.log( `Количество элементов: ${item.children[1].children.length}`);
+});
+
+
+
