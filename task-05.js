88727bfeec43e5f59e2ff109ba57215b14032525
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-11-26 19:05:08 +0200

    update task-05

--- task-05.js
+++ task-05.js
@@ -2,9 +2,12 @@ const inputNameRef = document.querySelector('#name-input');
 const outputNameRef = document.querySelector('#name-output');
 
 inputNameRef.addEventListener('input', event => {
-  outputNameRef.textContent = event.target.value;
+  event.target.value
+  ? (outputNameRef.textContent = event.target.value)
+  : (outputNameRef.textContent = 'незнакомец');
   
-  if (event.target.value === '') {
-    outputNameRef.textContent = 'незнакомец';
-  }
+  // outputNameRef.textContent = event.target.value;
+  // if (event.target.value === '') {
+  //   outputNameRef.textContent = 'незнакомец';
+  // }
 });
